0f8f3d6498b30e4d46b4793bc529b5ba346bb889
Giovanni Troisi <giovanni@tiledesk.com>
2025-06-20 11:47:42 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-const test1 = require("./test1");
-const test2 = require("./test2");
-
-console.log("Hello World from index.js");
-
-console.log(test1.method1());
-console.log(test2.method1());
+// First line
